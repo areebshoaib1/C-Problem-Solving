2d7847ac123e6ae3af67977bc2f0b8691e980388
areebshoaib1 <areebspecial1@gmail.com>
2026-02-07 18:32:37 +0500

    Create P1.c

---
+++ P1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    printf("Enter number :");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("Number is Even\n");
+    }
+    else
+    {
+        printf("Number is Odd\n");
+    }
+
+    return 0;
+}
