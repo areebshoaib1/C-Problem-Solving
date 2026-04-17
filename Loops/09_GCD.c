81a0e802ba7a471d91a5c38035595633a0f1967d
areebshoaib1 <areebspecial1@gmai.com>
2026-04-17 16:54:48 +0500

    Solve: Find GCD of two numbers

--- Loops/09_GCD.c
+++ Loops/09_GCD.c
@@ -8,56 +8,22 @@ int main()
 {
     int n1;
     int n2;
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int arr_1[50];
-    int arr_2[50];
-    printf("Enter first number: \n");
+    int gcd = 1;
+
+    printf("Enter first number: ");
     scanf("%d", &n1);
-    printf("Enter Second number: \n");
+    printf("Enter Second number: ");
     scanf("%d", &n2);
 
-    for (int i = 2; i < n1; i++)
-    {
+    int min = (n1 > n2) ? n1 : n2;
 
-        if (n1 % i == 0)
+    for (int i = 1; i <= min; i++)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
         {
-            arr_1[i] = i;
-        }
-        else
-        {
-            arr_1[i] = -1;
+            gcd = i;
         }
     }
 
-    for (int j = 2; j < n2; j++)
-    {
-        if (n2 % j == 0)
-        {
-            arr_2[j] = j;
-        }
-        else
-        {
-
-            arr_2[j] = -2;
-        }
-    }
-
-    for (int k = 2; k < n1 && k < n2; k++)
-    {
-        if (arr_1[k] == arr_2[k] && arr_1[k] != -1 && arr_2[k] != -2)
-        {
-
-            a = arr_1[k];
-            if (a > b)
-            {
-                b = a;
-            }
-        }
-    }
-    printf("Greatest common divisor is : %d\n", b);
-
-    return 0;
+    printf("Greatest common divisor is %d", gcd);
 }
-
