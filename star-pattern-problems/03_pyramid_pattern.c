913ee44d8aae7d0f7f1f83b31882d06d3fe13f8c
areebshoaib1 <areebspecial1@gmai.com>
2026-05-04 00:35:02 +0500

    Solve: Print pyramid pattern

---
+++ star-pattern-problems/03_pyramid_pattern.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+C program for :-
+
+Print pyramid pattern
+
+  *
+ ***
+*****
+
+Author : Muhammad Areeb Shoaib
+*/
+
+int main()
+{
+    int rows = 4;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
