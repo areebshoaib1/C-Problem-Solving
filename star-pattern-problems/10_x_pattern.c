ba88cc580a4d9b576d63b67f81b0871bf90f0542
areebshoaib1 <areebspecial1@gmai.com>
2026-05-07 19:58:19 +0500

    Solve: Print X pattern using stars.

---
+++ star-pattern-problems/10_x_pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+C program for :-
+Print X pattern using stars
+Author : Muhammad Areeb Shoaib
+*/
+
+int main()
+{
+    int rows = 5;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if (i == j || i + j == rows - 1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
